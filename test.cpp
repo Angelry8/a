22099c5e9578b1f980b97a9927d0035e89bcb45e
Angelry8 <proangel768@gmail.com>
2025-05-23 21:32:39 -0600

    se agrega la resta en el main

--- test.cpp
+++ test.cpp
@@ -4,18 +4,28 @@ using namespace std;
 double suma (double a, double b ){
     return a+b;
 }
-
+double resta (double c, double d ){
+    return c-d;
+}
 
 int main() {
 
 double a =0;
 double b =0;
-cout<< "Bienvenido!!" <<endl;
+double c =0;
+double d =0;
+cout<< "Bienvenido, a sumar!!" <<endl;
 cout<< "Ingrese el primer numero: " <<endl;
 cin >> a;
 cout<< "Ingrese el segundo numero: " <<endl;
 cin >> b;
 cout<< "La suma es: "<<double(suma(a,b)) <<endl;
+cout<< "Bienvenido, a restar!!" <<endl;
+cout<< "Ingrese el primer numero: " <<endl;
+cin >> c;
+cout<< "Ingrese el segundo numero: " <<endl;
+cin >> d;
+cout<< "La resta es: "<<double(resta(c,d)) <<endl;
 
 
 return 0;
